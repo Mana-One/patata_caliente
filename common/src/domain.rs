90b4fdf1d6a59200f3d73c9d9a3a8500985e82f6
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-06-26 01:14:08 +0200

    domain types

---
+++ common/src/domain.rs
@@ -0,0 +1,45 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+pub enum SubscribeError {
+    AlreadyRegistered, 
+    InvalidName
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct PublicPlayer {
+    name: String,
+    stream_id: String,
+    score: i32,
+    steps: u32,
+    is_active: bool,
+    total_used_time: f64
+}
+
+#[derive(Deserialize, Serialize)]
+pub enum ChallengeAnswer {
+    ChallengeName(ChallengeOutput)
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct ChallengeResult {
+    name: ChallengeAnswer,
+    next_target: String
+}
+
+#[derive(Deserialize, Serialize)]
+pub enum ChallengeValue {
+    Unreachable,
+    Timeout,
+    BadResult { used_time: f64, next_target: String },
+    Ok { used_time: f64, next_target: String }
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct ReportedChallengeResult {
+    name: String,
+    value: JobValue
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct PublicLeaderBoard(Vec<PublicPlayer>);
